da76f5f5176792b9bc862b20a2543215e0f364e6
Dillon Buchanan <thedillonb@gmail.com>
2014-05-03 18:31:11 -0400

    Initial commit

---
+++ CodeStash.iOS/AppDelegate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace CodeStash.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        // class-level declarations
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
+
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+
+        public override void FinishedLaunching(UIApplication application)
+        {
+            Window = new UIWindow();
+            Window.MakeKeyAndVisible();
+        }
+    }
+}
+
